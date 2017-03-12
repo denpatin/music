9a5006fcac64abd1784099bd0cd379ea80cb214b
Den Patin <denpatin@gmail.com>
2017-03-12 20:42:33 +0300

    Update as of 2017-03-12

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tree ~/Music/iTunes/iTunes\ Media/Music -N > README
+
+if [[ `git status -s | grep README` ]]; then
+  git add -A
+  git commit -am "Update as of `date +%Y-%m-%d`"
+  git push origin master
+else
+  echo "No changes"
+fi

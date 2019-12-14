1e52c1f859cc6c058f588fd1433e5b3991562400
Den Patin <denpatin@gmail.com>
2019-12-14 21:29:37 +0300

    Update as of 2019-12-14

--- update.sh
+++ update.sh
@@ -2,10 +2,11 @@
 
 tree ~/Music/Music/Media/Music -N | sed 's/\.m[p4][3a]$//' > README
 
+export GPG_TTY=$(tty)
 if git status -s | grep -q README
 then
   git add -A
-  git commit -am "Update as of $(date +%Y-%m-%d)"
+  git commit -S -m "Update as of $(date +%Y-%m-%d)"
   git push origin master
 else
   echo "No changes"

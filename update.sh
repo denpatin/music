9078b14f9a0af2dd8e52b596d50208b8e10ce425
Den Patin <hi@dpat.in>
2026-06-07 01:06:14 +0800

    Update as of 2026-06-07

--- update.sh
+++ update.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
-set -euo pipefail
+#!/usr/bin/env fish
 
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$REPO_DIR"
+set REPO_DIR (cd (dirname (status --current-filename)); and pwd)
+cd $REPO_DIR
 
 ruby scripts/08_make_readme.rb > README
 
-export GPG_TTY=$(tty)
+set -x GPG_TTY (tty)
 if git status --porcelain | grep -q .
-then
-  BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-  git add -A
-  git commit -S -m "Update as of $(date +%Y-%m-%d)"
-  git push origin "$BRANCH"
+    set BRANCH (git rev-parse --abbrev-ref HEAD)
+    git add -A
+    git commit -S -m "Update as of "(date +%Y-%m-%d)
+    git push origin $BRANCH
 else
-  echo "No changes"
-fi
+    echo "No changes"
+end
